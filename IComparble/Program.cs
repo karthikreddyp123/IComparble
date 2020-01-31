6747344fa014bde93d7314387bd5ea0fd46e9ee8
Karthik Patlolla <Karthik_Patlolla@epam.com>
2020-01-31 14:45:03 +0530

    Add project files.

---
+++ IComparble/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IComparble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student[] arrStudent = new Student[] { new Student() { Name = "Karthik" },
+            new Student() { Name = "Rahul" },
+            new Student() { Name = "Vivek" },
+            new Student() { Name = "Aditya" }};
+
+            Array.Sort(arrStudent);
+            foreach(Student i in arrStudent)
+            {
+                Console.WriteLine(i.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+    public class Student : IComparable
+    {
+        public string Name { get; set; }
+        public int CompareTo(object obj)
+        {
+            Student stuObj = obj as Student;
+            if (stuObj != null)
+            {
+                return this.Name.CompareTo(stuObj.Name);
+            }
+            return -1;
+        }
+    }
+}
